eea7df94dcf6430564c0b7b11219075f1a4e2d69
Imran Nur <imrannurint@gmail.com>
2026-04-30 11:46:41 +0600

    update env example

--- app/core/config.py
+++ app/core/config.py
@@ -5,6 +5,9 @@ class Settings(BaseSettings):
     DATABASE_URL: str = "postgresql://user:password@localhost:5432/mie_search_index_db"
     RABBITMQ_HOST: str = "localhost"
     RABBITMQ_QUEUE: str = "global_search_updates"
+
+    REDIS_HOST: str = "127.0.0.1"
+    REDIS_PORT: int = 6379
     
     JWT_SECRET: str = "your-secret-key"
     ALGORITHM: str = "HS256"
